75126b8fb2e89c022b236c826b5eece59c37390a
Pedro G. Bello <soy.pedrobello99@gmail.com>
2025-02-26 19:48:19 -0300

    Created  function.
    - Function uses the Luhn's algorithm to check whether a credit card is valid or not.

--- week_01/02_credit/credit.c
+++ week_01/02_credit/credit.c
@@ -1,24 +1,48 @@
+/*
+    CS50x - Intro to Computer Science
+    Programming in C
+    WEEK 1 - Problem Set: Credit
+    --------------------------------
+
+        The `credit` program determines whether a provided credit card number is valid according to Luhn’s algorithm.
+
+    The program prompts the user for a credit card number and then reports (via `printf`) whether it is a valid American Express, MasterCard, or Visa card number, according to the definitions of each’s format.
+
+    American Express uses 15-digit numbers, starting with 34 or 37.
+    MasterCard uses 16-digit numbers, starting with 51, 52, 53, 54, or 55.
+    Visa uses 13- and 16-digit numbers, starting with 4.
+
+    The program’s output should be exactly as follows:
+    
+    - `AMEX\n` for an American Express card
+    - `MASTERCARD\n` for a MasterCard
+    - `VISA\n` for a Visa card
+    - `INVALID\n` for an invalid card
+
+    Luhn’s Algorithm:
+    1. Multiply every other digit by 2, starting with the number’s second-to-last digit.
+    2. Add those products' digits together.
+    3. Add the sum to the sum of the digits that weren’t multiplied by 2.
+    4. If the total’s last digit is 0, the number is valid!
+*/
+
 #include <stdio.h>
+#include <stdbool.h>
 
 int cc_length(long int cc_number);
+bool check_cc_validity(long int cc_number);
 
 int main(void)
 {
-    // Luhn's algorithm.
     // Prompt user for credit card number (without spaces or dashes).
     long int cc_number;
     
     printf("Enter credit card number: ");
     scanf("%ld", &cc_number);
     int length = cc_length(cc_number);
-    printf("Length: %d\n", length);
-    // Check if the credit card number is valid.
-    // If valid, print the type of credit card.
-    // If invalid, print "INVALID".
-    // American Express: 15 digits, starts with 34 or 37.
-    // MasterCard: 16 digits, starts with 51, 52, 53, 54, or 55.
-    // Visa: 13 or 16 digits, starts with 4. 
-    // Otherwise, print "INVALID".
+    bool is_valid = check_cc_validity(cc_number);
+
+    is_valid ? printf("Card %ld is valid!\n", cc_number) : printf("INVALID\n");
 
     return 0;
 }
@@ -33,4 +57,31 @@ int cc_length(long int cc_number)
         length++;
     }
     return length;
+}
+
+// Credit card type:
+bool check_cc_validity(long int cc_number)
+{
+    // Check if the credit card number is valid.
+    int sum = 0;
+    int removed_digit = 0;
+
+    while(cc_number > 0)
+    {
+        // Get the last digit of the credit card number.
+        int digit = cc_number % 10;
+        removed_digit++;
+
+        if (removed_digit % 2 == 0)
+        {
+            sum += (digit * 2 > 9) ? (digit * 2 - 9) : digit * 2;
+        }
+        else
+        {
+            sum += digit;
+        }
+        // Remove the last digit of the credit card number.
+        cc_number /= 10;
+    }
+    return (sum % 10 == 0);
 }
